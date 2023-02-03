ccc6aa69289c287fc4bca1c3473aef366dbe1791
Renan Cavichi <renancavichi@gmail.com>
2023-02-03 17:56:15 -0300

    create web server with node

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const http = require('node:http');
+
+const hostname = 'localhost';
+const port = 3100;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
