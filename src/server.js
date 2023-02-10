0fad02ccef017416e9c25a335e9059ade031e961
Renan Cavichi <renancavichi@gmail.com>
2023-02-10 17:17:21 -0300

    how to require and response json file

--- src/server.js
+++ src/server.js
@@ -1,27 +1,30 @@
-const http = require('node:http');
-const fs = require('fs')
+const express = require('express')
+const listaCursos = require('./db/cursos.json')
+const app = express()
+const port = 3100
 
-const hostname = 'localhost';
-const port = 3100;
+app.get('/', (req, res) => {
+  const msg = [{nome: 'LP2'},{nome: 'PJ3'}]
+  res.json(msg)
+})
 
-const server = http.createServer((req, res) => {
-  console.log('URL: ' + req.url)
-  if(req.url === '/'){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('src/views/index.html', (error, codigo) => {
-      res.end(codigo);
-    })
-  } else if(req.url === '/cursos'){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('src/views/cursos.html', (error, codigo) => {
-      res.end(codigo);
-    })
-  }
+app.get('/cursos', (req, res) => {
+    res.json(listaCursos)
+})
 
-});
+app.post('/cursos', (req, res) => {
+  res.json({message: 'POST Cursos JSON!'})
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.put('/cursos', (req, res) => {
+  res.send('Fiz um update no Curso!')
+})
+
+app.all('*', (req, res) =>{
+  res.send('404 Rota não encontrada!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
