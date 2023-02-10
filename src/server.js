7d2167a5293e9eefab59dcd2783fcfec60739718
Renan Cavichi <renancavichi@gmail.com>
2023-02-10 15:25:50 -0300

    add routes to server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 const http = require('node:http');
+const fs = require('fs')
 
 const hostname = 'localhost';
 const port = 3100;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+  console.log('URL: ' + req.url)
+  if(req.url === '/'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('src/views/index.html', (error, codigo) => {
+      res.end(codigo);
+    })
+  } else if(req.url === '/cursos'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('src/views/cursos.html', (error, codigo) => {
+      res.end(codigo);
+    })
+  }
+
 });
 
 server.listen(port, hostname, () => {
