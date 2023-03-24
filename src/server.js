54c98e109846104cdc676275193e9a4ec3bdc8bf
Renan Cavichi <renancavichi@gmail.com>
2023-03-24 17:00:11 -0300

    add middlewares logger and errorhandler

--- src/server.js
+++ src/server.js
@@ -1,23 +1,25 @@
-//const express = require('express')
 import express from 'express'
 import courseRoutes from './routes/courseRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import { SERVER } from './config.js'
+import errorHandler from './middlewares/errorHandler.js'
+import logger from './middlewares/logger.js'
 
 const app = express()
 const port = SERVER.PORT
 
+app.use(logger)
 app.use(express.json())
-//app.use(express.urlencoded({ extended: false })) // form urlenconded
-
-//TODO: Tratar erro de json inválido
+//app.use(express.urlencoded({ extended: false })) // get form data urlenconded
 
 app.use('/course', courseRoutes)
 app.use('/user', userRoutes)
 
 app.all('*', (req, res) => {
-  res.status(404).send('404 Rota não encontrada!')
+  res.status(404).json({ message: '404 Rota não encontrada...' })
 })
+
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
