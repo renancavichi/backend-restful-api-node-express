3fd3415093b827f9ea68dafc0c70a5c1f3770302
Renan Cavichi <renancavichi@gmail.com>
2023-03-31 17:01:15 -0300

    add user schema error messages

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,20 +1,44 @@
 import con from '../db/dbConnection.js'
 import { z } from 'zod'
 
+//TODO Testar regex com zod e ChatGPT
+
 const userSchema = z.object({
-  id: z.number().optional(),
-  nome: z.string().min(3).max(50),
+  id:
+    z.number({ message: "ID deve ser um valor numérico." })
+      .optional(),
+  nome:
+    z.string({
+      required_error: "Nome é obrigatória.",
+      invalid_type_error: "Nome deve ser uma string.",
+    })
+      .min(3, { message: "Nome deve ter no mínimo 3 caracteres." })
+      .max(100, { message: "Nome deve ter no máximo 100 caracteres." }),
   email:
-    z.string({ message: "Email Inválido" })
-      .email({ message: "Email Inválido" })
-      .min(5, { message: "O email deve ter ao menos 5 Caracteres" })
-      .max(50),
-  senha: z.string().min(3).max(50),
-  avatar: z.string()
+    z.string({
+      required_error: "Email é obrigatória.",
+      invalid_type_error: "Email deve ser uma string.",
+    })
+      .email({ message: "Email Inválido." })
+      .min(5, { message: "O email deve ter ao menos 5 caracteres." })
+      .max(200, { message: "Email deve ter no máximo 200 caracteres." }),
+  senha:
+    z.string({
+      required_error: "Senha é obrigatória.",
+      invalid_type_error: "Senha deve ser uma string.",
+    })
+      .min(6, { message: "Senha deve ter no mínimo 6 caracteres." })
+      .max(256, { message: "Senha deve ter no máximo 256 caracteres." }),
+  avatar:
+    z.string({
+      required_error: "Avatar é obrigatória.",
+      invalid_type_error: "Avatar deve ser uma string.",
+    })
+      .url({ message: "Avatar deve ser uma URL válida." })
 })
 
 export const validateUser = (user) => {
-  return userSchema.parse(user)
+  return userSchema.safeParse(user)
 }
 
 export const listAllUsers = (callback) => {
