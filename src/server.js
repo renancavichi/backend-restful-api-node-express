065e7290a9ec6c1ec0d9da76152c2e2f3713178b
Renan Cavichi <renancavichi@gmail.com>
2023-04-14 16:38:15 -0300

    add cors middleware

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import courseRoutes from './routes/courseRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import errorHandler from './middlewares/errorHandler.js'
@@ -9,6 +10,7 @@ const app = express()
 const port = SERVER.PORT
 
 app.use(logger)
+app.use(cors())
 app.use(express.json())
 //app.use(express.urlencoded({ extended: false })) // get form data urlenconded
 
